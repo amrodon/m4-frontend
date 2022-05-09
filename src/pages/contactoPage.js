0493a959cd811fd65b1194573421e8bf889e15c0
Alejandro Rodón <amrodon@gmail.com>
2022-05-08 21:21:39 -0300

    cambios

--- src/pages/contactoPage.js
+++ src/pages/contactoPage.js
@@ -1,36 +1,36 @@
 import '../styles/components/pages/contactoPage.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 
-const contactoPage = (props) => {
+const ContactoPage = (props) => {
 
-    const initialForm={
-        nombre:'',
-        email:'',
-        telefono:'',
-        mensaje:''
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
     }
-    const [sending, setSending]=useState(false);
-    const [msg,setMsg]=useState('');
-    const [formData, setFormData]=useState(initialForm);
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
 
-    const handleChange=e=>{
-        const{name,value}=e.target;
-        setFormData(oldData=>({
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
             oldData,
-            [name]:value
+            [name]: value
         }))
     }
-    const handleSubmit=async e=>{
+    const handleSubmit = async e => {
         e.preventDefault();
-        SetMsg('');
+        setMsg('');
         setSending(true)
-        const response=await
-        axios.post('http://localhost:3000/api/contacto',formData);
+        const response = await
+            axios.post('http://localhost:3000/api/contacto', formData);
         setSending(false);
         setMsg(response.data.message);
-        if (response.data.error === false){
+        if (response.data.error === false) {
             setFormData(initialForm)
         }
     }
@@ -38,26 +38,26 @@ const contactoPage = (props) => {
         <main className='holder'>
             <div className='columna contacto'>
                 <h2>Complete el siguiente formulario</h2>
-                <form action='/contacto' method='post' className='formulario' onSubmit={onSubmit}>
+                <form action='/contacto' method='post' className='formulario' onSubmit={handleSubmit}>
                     <p>
                         <label>Nombre</label>
                         <input type='text' name='nombre' value={formData.nombre}
-                        onChange={handleChange}/>
+                            onChange={handleChange} />
                     </p>
                     <p>
                         <label>Email</label>
                         <input type='text' name='email' value={formData.email}
-                        onChange={handleChange} />
+                            onChange={handleChange} />
                     </p>
                     <p>
                         <label>Teléfono</label>
-                        <input type='text' name='teléfono' value={formData.teléfono}
-                        onChange={handleChange} />
+                        <input type='text' name='teléfono' value={formData.telefono}
+                            onChange={handleChange} />
                     </p>
                     <p>
                         <label>Comentario</label>
-                        <textarea name='mensaje'value={formData.mensaje}
-                        onChange={handleChange} ></textarea>
+                        <textarea name='mensaje' value={formData.mensaje}
+                            onChange={handleChange} ></textarea>
 
                     </p>
                     <p className='centrar'><input type='submit'
@@ -65,8 +65,8 @@ const contactoPage = (props) => {
                     </p>
 
                 </form>
-                {sending ? <p>Enviando...</p>:null}
-                {msg ? <p>{msg}</p>:null}
+                {sending ? <p>Enviando...</p> : null}
+                {msg ? <p>{msg}</p> : null}
             </div>
             <div className='columna datos'>
                 <h2>Otras vias de contacto</h2>
@@ -84,4 +84,4 @@ const contactoPage = (props) => {
 
     );
 }
-export default contactoPage;
+export default ContactoPage;
